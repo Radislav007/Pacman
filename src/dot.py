f875d97fa1a3016d47aad19cc3506d7ffc4457ff
Yura Krutsko <krutsko90@gmail.com>
2025-05-16 13:19:34 +0300

    Save current changes before switching branch

--- src/dot.py
+++ src/dot.py
@@ -1,19 +1,20 @@
 import pygame
 
 class Dot(pygame.sprite.Sprite):
-    def __init__(self, x, y, size):
+    def __init__(self, x, y, size, is_power=False):
         super().__init__()
-        self.image = pygame.Surface((size, size))
-        self.image.fill((255, 255, 0))  # Жовта крапка
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.alive = True  # Крапка активна
+        self.is_power = is_power
+        self.size = size
+        self.image = pygame.Surface((size, size), pygame.SRCALPHA)
+
+        if self.is_power:
+            pygame.draw.circle(self.image, (255, 255, 0), (size // 2, size // 2), size // 2)  # Жовта велика крапка
+        else:
+            pygame.draw.circle(self.image, (255, 255, 255), (size // 2, size // 2), size // 6)  # Біла маленька крапка
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x * size + size // 2 - self.rect.width // 2,
+                             y * size + size // 2 - self.rect.height // 2)
 
     def draw(self, screen):
-        if self.alive:
-            screen.blit(self.image, self.rect)
-
-    def update(self, pacman):
-        if self.alive and self.rect.colliderect(pacman.rect):
-            self.kill()
-            self.alive = False
-
+        screen.blit(self.image, self.rect)
